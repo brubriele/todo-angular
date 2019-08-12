0a382e9b79787f8d2bf09b421f8efe1a40ec85ce
Bruna G Paula <brunagabriele.paula@gmail.com>
2019-08-12 15:52:57 -0300

    [start-app] Iniciliza aplicação, define variáveis e tipos e métodos construtores

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Todo } from 'src/models/todo.model';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app-root', //<app-root></app-root>
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo';
+  public todos: Todo[] = [];
+  public title: String = 'Minhas tarefas';
+
+  //Incluindo Tasks - quando componente iniciado
+  constructor() {
+    this.todos.push(new Todo(1, 'Passear com o cachorro', false));
+    this.todos.push(new Todo(2, 'Ir ao supermecado', false));
+    this.todos.push(new Todo(3, 'Cortar o cabelo', true));
+  }
+
+  //Removvendo itens da lista
 }
